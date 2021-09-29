f2305ed81cc7c82afd3d15ed6f56108d7b0a1045
MDLauriers36 <90851590+MDLauriers36@users.noreply.github.com>
2021-09-28 21:03:15 -0400

    Add files via upload

---
+++ Git/html/script.js
@@ -0,0 +1,57 @@
+let premier_chapitre = {
+    subtitle: "Sed ut perspiciatis",
+    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    img: af
+  };
+  let chaptersObj = {
+      slug : "l'arrivée",
+  }
+
+  let aucune_aide = {
+    
+    text:"Le commis ne peut pas vous aider",
+    img: af,
+    options = ["Douter de ses compétences", "Vous plaindre", "Rien faire"]
+  };
+
+  let deux_mille = {
+    
+    text:"Le commis vous affirme qu'il a plus de 2000 produits différents dans son magasin et ne peut pas tous les connaitre. Que voulez-vous faire?",
+    img: af,
+    options = ["Passer à autre chose", "Appeler le gérant", "Saluer l'employé et partir"]
+    
+  };
+
+  let autre_chose = {
+    
+    text:"Partie perdue. Retour à la première question (Rappelez-vous que vous devez embeter le plus de personnes possibles.",
+    img: af,
+    options = ["Douter de ses compétences", "Vous plaindre", "Rien faire"]
+  };
+  let gerant_aide = {
+    
+    text:"Le gérant vous a aidé à trouver votre produit.",
+    img: af,
+    options = ["Le remercier", "Dire que son employé doit être mieux formé", "Rien faire"]
+  };
+
+  let commis_calme = {
+    
+    text:"Le commis vous demande de vous calmer. Que voulez-vous faire?",
+    img: af,
+    options = ["Jouer la victime", "Hausser le ton", "Appeler le gérant"]
+  };
+
+  let jouer_victime = {
+    
+    text:"Le commis vous propose d'appeler son gérant",
+    img: af,
+    options = ["S'excuser", "Partir vers les caisses", "Rien faire"]
+  };
+
+  let partir_caisses = {
+    
+    text:"Alors que vous voulez vous faire rembourser un produit que vous avez consommé, la caissière vous dit qu'il est impossible de vous rembourser. Que voulez-vous faire?",
+    img: af,
+    options = ["Douter de ses compétences", "Vous plaindre", "Rien faire"]
+  };
---
+++ Git/vous-etes-le-heros/README.md
@@ -0,0 +1,9 @@
+# vous-etes-le-heros
+Travail 1 
+Titre : Libère ton stress!
+**Genre**: Comédie, 
+**Inspiration** : Sans objets 
+
+<h2>Diagramme<h2> 
+![E-field-distances](/../assets/diagrammeFini.jpg)
+

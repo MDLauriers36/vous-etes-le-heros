c1dde1093b92c2c8adfe3aa36b19e43401fa3c1d
MDLauriers36 <90851590+MDLauriers36@users.noreply.github.com>
2021-09-30 00:42:44 -0400

    Update script.js

--- Git/html/script.js
+++ Git/html/script.js
@@ -1,7 +1,6 @@
 
 let chaptersObj = {
 
-
   premier_chapitre : {
     subtitle: "L'arrivée",
     text:"Vous arrivez dans un magasin grande surface alors que vous avez massé un très mauvaise journée. Vous devez donc relâcher votre stress sur des employés qui n'ont rien demandé. Saurez-vous bien vous déstresser?",
@@ -13,52 +12,52 @@ let chaptersObj = {
     subtitle: "L'incapable",
     text:"Le commis ne peut pas vous aider",
     img: ("images/Gif/giphy.gif"),
-    options : [Douter, Plaindre, Rien_faire]
+    options : [Douter, Plaindre, Rien_faire],
   },
 
   deux_mille : {
   subtitle: "L'excuse du commis",
   text:"Le commis vous affirme qu'il a plus de 2000 produits différents dans son magasin et ne peut pas tous les connaitre. Que voulez-vous faire?",
   img: ("images/Gif/giphy.gif"),
-  options : ["Passer à autre chose", "Appeler le gérant", "Saluer l'employé et partir"]
+  options : ["Passer à autre chose", "Appeler le gérant", "Saluer l'employé et partir"],
   
 },
   autre_chose : {
     subtitle: "BOOOOOO!",
     text:"Partie perdue. Retour à la première question (Rappelez-vous que vous devez embêter le plus de personnes possibles.",
     img: ("images/Gif/giphy.gif"),
-    options : [Douter, Plaindre, Rien_faire]
+    options : [Douter, Plaindre, Rien_faire],
   },
 
   gerant_aide : {
   subtitle: "Enfin de l'aide",
   text:"Le gérant vous a aidé à trouver votre produit.",
   img: ("images/Gif/giphy.gif"),
-  options : [remercier, mieux_formé, Rien_faire]
+  options : [remercier, mieux_formé, Rien_faire],
 },
   commis_calme : {
     subtitle: "L'audace du commis",
     text:"Le commis vous demande de vous calmer. Que voulez-vous faire?",
     img: ("images/Gif/giphy.gif"),
-    options : [victime, Hausser_ton, Appeler_gérant]
+    options : [victime, Hausser_ton, Appeler_gérant],
   },
   jouer_victime : {
     subtitle: "La carte de la victime",
     text:"Le commis vous propose d'appeler son gérant",
     img: ("images/Gif/giphy.gif"),
-    options : [excuser, Go_caisses, Rien_faire]
+    options : [excuser, Go_caisses, Rien_faire],
   },
   partir_caisses : {
     subtitle: "Pfff...",
     text:"Alors que vous voulez vous faire rembourser un produit que vous avez consommé, la caissière vous dit qu'il est impossible de vous rembourser. Que voulez-vous faire?",
     img: ("images/Gif/giphy.gif"),
-    options : [Partir, Demander_gérant, Rien_faire]
+    options : [Partir, Demander_gérant, Rien_faire],
   },
   remboursement : {
     subtitle: "Vraiments tous incapables",
     text:"Le gérant vous affirme qu'il est impossible de vous rembourser. Que voulez-vous faire?",
     img: ("images/Gif/giphy.gif"),
-    options : [Partir, Jamais_revenir, Insulter]
+    options : [Partir, Jamais_revenir, Insulter],
   },
   //
   //

fb6214f9850743ae1abdd15180ef2d55e5ebd2a4
MDLauriers36 <90851590+MDLauriers36@users.noreply.github.com>
2021-09-30 00:13:53 -0400

    Update script.js

--- Git/html/script.js
+++ Git/html/script.js
@@ -1,73 +1,136 @@
 
 let chaptersObj = {
+              //Chapitres
   premier_chapitre : {
-    subtitle: "Sed ut perspiciatis",
-    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    subtitle: "L'arrivée",
+    text:"Vous arrivez dans un magasin grande surface alors que vous avez massé un très mauvaise journée. Vous devez donc relâcher votre stress sur des employés qui n'ont rien demandé. Saurez-vous bien vous déstresser?",
     img: ("images/Gif/giphy.gif"),
+    options : [, commencer, ]
   }
     ,
   aucune_aide : {
-    subtitle: "Sed ut perspiciatis",
+    subtitle: "L'incapable",
     text:"Le commis ne peut pas vous aider",
     img: ("images/Gif/giphy.gif"),
-    options : ["Douter de ses compétences", "Vous plaindre", "Rien faire"]
+    options : [Douter, Plaindre, Rien_faire]
   },
 
   deux_mille : {
-  subtitle: "Sed ut perspiciatis",
+  subtitle: "L'excuse du commis",
   text:"Le commis vous affirme qu'il a plus de 2000 produits différents dans son magasin et ne peut pas tous les connaitre. Que voulez-vous faire?",
   img: ("images/Gif/giphy.gif"),
   options : ["Passer à autre chose", "Appeler le gérant", "Saluer l'employé et partir"]
   
 },
   autre_chose : {
-    subtitle: "Sed ut perspiciatis",
-    text:"Partie perdue. Retour à la première question (Rappelez-vous que vous devez embeter le plus de personnes possibles.",
+    subtitle: "BOOOOOO!",
+    text:"Partie perdue. Retour à la première question (Rappelez-vous que vous devez embêter le plus de personnes possibles.",
     img: ("images/Gif/giphy.gif"),
-    options : ["Douter de ses compétences", "Vous plaindre", "Rien faire"]
+    options : [Douter, Plaindre, Rien_faire]
   },
 
   gerant_aide : {
-  subtitle: "Sed ut perspiciatis",
+  subtitle: "Enfin de l'aide",
   text:"Le gérant vous a aidé à trouver votre produit.",
   img: ("images/Gif/giphy.gif"),
-  options : ["Le remercier", "Dire que son employé doit être mieux formé", "Rien faire"]
+  options : [remercier, mieux_formé, Rien_faire]
 },
   commis_calme : {
-    subtitle: "Sed ut perspiciatis",
+    subtitle: "L'audace du commis",
     text:"Le commis vous demande de vous calmer. Que voulez-vous faire?",
     img: ("images/Gif/giphy.gif"),
-    options : ["Jouer la victime", "Hausser le ton", "Appeler le gérant"]
+    options : [victime, Hausser_ton, Appeler_gérant]
   },
   jouer_victime : {
-    subtitle: "Sed ut perspiciatis",
+    subtitle: "La carte de la victime",
     text:"Le commis vous propose d'appeler son gérant",
     img: ("images/Gif/giphy.gif"),
-    options : ["S'excuser", "Partir vers les caisses", "Rien faire"]
+    options : [excuser, Go_caisses, Rien_faire]
   },
   partir_caisses : {
-    subtitle: "Sed ut perspiciatis",
+    subtitle: "Pfff...",
     text:"Alors que vous voulez vous faire rembourser un produit que vous avez consommé, la caissière vous dit qu'il est impossible de vous rembourser. Que voulez-vous faire?",
     img: ("images/Gif/giphy.gif"),
-    options : ["Partir", "Demander le gérant", "Rien faire"]
+    options : [Partir, Demander_gérant, Rien_faire]
   },
   remboursement : {
-    subtitle: "Sed ut perspiciatis",
+    subtitle: "Vraiments tous incapables",
     text:"Le gérant vous affirme qu'il est impossible de vous rembourser. Que voulez-vous faire?",
     img: ("images/Gif/giphy.gif"),
-    options : ["Partir", "Demander le gérant", "Rien faire"]
+    options : [Partir, Jamais_revenir, Insulter]
+  },
+  //
+  //
+  //
+  commencer : {
+    text : "Commencer la partie",
+    action : (goToChapter(deux_mille))
+  },
+  Plaindre : {
+    text: "Douter des compétences de l'employé",
+    action : (goToChapter(deux_mille))
+  },
+  Douter : {
+    text: "Douter des compétences de l'employé",
+    action : (goToChapter(deux_mille))
+  },
+  mieux_formé : {
+    text: "Dire que son employé devrait être mieux formé",
+    action : (goToChapter(autre_chose))
+  },
+  remercier : {
+    text: "Le remercier",
+    action : (goToChapter(autre_chose))
+  },
+  Appeler_gérant : {
+    text : "Appeler_gérant",
+    action : (goToChapter(autre_chose))
+  },
+  Hausser_ton : {
+    text : "Hausser le ton",
+    action : (goToChapter(autre_chose))
+
+  },
+  victime : {
+    text : "Jouer à la victime",
+    action : (goToChapter(jouer_victime))
+  },
+  Rien_faire : {
+    text : "Rien faire",
+    action : (goToChapter(autre_chose))
   },
 
-  partir : {
+  Go_caisses : {
+    text : "Partir vers les caisses",
+    action : (goToChapter(partir_caisses))
+  },
+  excuser : {
+    text : "S'excuser",
+    action : (goToChapter(autre_chose))
+  },
+  Demander_gérant : {
+    text : "Demander à parler au gérant",
+    action : (goToChapter(premier_chapitre))
+  },
+
+  Partir : {
     text : "Partir",
-    
+    action : (goToChapter(premier_chapitre))
+  },
+  
+  Jamais_revenir : {
+    text : "Dire que vous n'allez plus jamais revenir",
+    action : (goToChapter(premier_chapitre)),
+  },
+  Insulter : {
+    text : "Insulter toutes les personnes qui se trouvent devant vous",
+    action : (goToChapter(premier_chapitre)),
   }
   
 }
-console.log(chaptersObj);
 
 function goToChapter(chapterName){
-  let chapitre = chapterName;
+  let chapitre = chaptersObj[chapterName];
   console.log(chapitre.subtitle);
   console.log(chapitre.text);
   

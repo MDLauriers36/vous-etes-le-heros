deb5ccb165b257fac320716e76df22366ecfdc55
MDLauriers36 <90851590+MDLauriers36@users.noreply.github.com>
2021-10-16 20:35:35 -0400

    Update script.js

--- Git/html/script.js
+++ Git/html/script.js
@@ -228,8 +228,8 @@ function goToChapter(chapterName) {
   let text = document.querySelector(".txt");
   let image = document.querySelector(".visuel");
   let choices = document.querySelector(".boutons-ligne1");
-  chapter.innerHTML = chaptersObj[chapterName].subtitle;
-  text.innerHTML = chaptersObj[chapterName].text;
+  chapter.innerText = chaptersObj[chapterName].subtitle;
+  text.innerText = chaptersObj[chapterName].text;
   image.innerHTML = `<img src="${chaptersObj[chapterName].img}" alt="chapter_img" />`;
   let txtButton = "";
   for (
